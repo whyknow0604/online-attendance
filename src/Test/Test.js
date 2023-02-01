708551785700bfb394c7868c0cc4ccfb8a612db6
MINO <whyknow0604@naver.com>
2023-02-01 12:21:30 +0900

    MatirialUI Test Success

---
+++ src/Test/Test.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Button from "@mui/material/Button";
+
+const Test = () => {
+  return <Button variant="contained" color="secondary">프로젝트 성공 기원</Button>;
+};
+
+export default Test;
